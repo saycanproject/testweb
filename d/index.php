24b3c71f18a36b7f77a70cfb4d5e7fd1eb7f36bb
saycanproject <125728055+saycanproject@users.noreply.github.com>
2023-05-19 00:57:24 +0800

    Update index.php

--- d/index.php
+++ d/index.php
@@ -1,1 +1,26 @@
 
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+session_start();
+
+require_once 'router.php';  // make sure router.php is in the same directory
+
+if (isset($_SESSION['user'])) {
+    echo 'Welcome ' . $_SESSION['user']['username'];
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <a href="index.php?action=login">Login</a>
+    <a href="index.php?action=register">Register</a>
+</body>
+</html>
